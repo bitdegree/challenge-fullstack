ed96e24f255bffd561403b038e837431577d25ad
Softelicious <dominykas.rakunas@gmail.com>
2019-11-02 19:20:39 +0200

    boilerplate with laravel, vue and scss

---
+++ resources/js/app.js
@@ -0,0 +1,10 @@
+
+require('./bootstrap');
+
+window.Vue = require('vue');
+
+Vue.component('comment-box', require('./components/comment_box/CommentBox.vue').default);
+
+const app = new Vue({
+    el: '#app',
+});
---
+++ resources/js/components/comment_box/CommentBox.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="comment-box">
+        <p> A comment box</p>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'commentbox',
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
+<style lang="scss" scoped>
+    @import "_comment_box.scss";
+</style>
