2afb5ef2e77ef8b286438fb67c9d672618ee69e9
dovydasvg <dovydasvg@yahoo.fr>
2021-04-20 16:39:17 +0300

    Add show_all method

--- laravel-vue-spa/app/Http/Controllers/CommentController.php
+++ laravel-vue-spa/app/Http/Controllers/CommentController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Comment;
 
 class CommentController extends Controller
 {
-    //
+
+    public function show_all()
+    {
+        return response(Comment::all(),201);
+    }
 }
